66696eced7d91a5fb0b8c1cd0430c7bc2f6ce22f
alekseevayana <alekseevayana@bk.ru>
2021-08-25 14:43:57 -0700

    btn active

--- src/components/ButtonsList.js
+++ src/components/ButtonsList.js
@@ -3,25 +3,22 @@ import Button from "./Button";
 export default function ButtonsList({ onClick }) {
   return (
     <>
-      <Button value="1" className="item" onClick={onClick} />
-      <Button value="2" className="item " onClick={onClick} />
-      <Button value="3" className="item " onClick={onClick} />
-      <Button value="+" className="item" onClick={onClick} />
-
-      <Button value="4" className="item " onClick={onClick} />
-      <Button value="5" className="item " onClick={onClick} />
-      <Button value="6" className="item " onClick={onClick} />
-      <Button value="-" className="item" onClick={onClick} />
-
-      <Button value="7" className="item " onClick={onClick} />
-      <Button value="8" className="item " onClick={onClick} />
-      <Button value="9" className="item " onClick={onClick} />
-      <Button value="*" className="item" onClick={onClick} />
-
-      <Button value="c" className="item " onClick={onClick} />
-      <Button value="0" className="item " onClick={onClick} />
-      <Button value="=" className="item" onClick={onClick} />
-      <Button value="/" className="item" onClick={onClick} />
+      <Button value="1" className="item button" onClick={onClick} />
+      <Button value="2" className="item button " onClick={onClick} />
+      <Button value="3" className="item button " onClick={onClick} />
+      <Button value="+" className="item button" onClick={onClick} />
+      <Button value="4" className="item button " onClick={onClick} />
+      <Button value="5" className="item button " onClick={onClick} />
+      <Button value="6" className="item button " onClick={onClick} />
+      <Button value="-" className="item button" onClick={onClick} />
+      <Button value="7" className="item button " onClick={onClick} />
+      <Button value="8" className="item button " onClick={onClick} />
+      <Button value="9" className="item button " onClick={onClick} />
+      <Button value="*" className="item button" onClick={onClick} />
+      <Button value="c" className="item button" onClick={onClick} />
+      <Button value="0" className="item button" onClick={onClick} />
+      <Button value="=" className="item button" onClick={onClick} />
+      <Button value="/" className="item button" onClick={onClick} />
     </>
   );
 }
